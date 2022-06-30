54ddd9fdf12c92513eebf560ff8ab6c69de0b818
LearnerRepo <aii1126@dlsud.edu.ph>
2022-06-30 20:04:04 +0800

    DFA fixes

--- Automata GUI/Assets/Scripts/DFA.cs
+++ Automata GUI/Assets/Scripts/DFA.cs
@@ -4,7 +4,12 @@ using UnityEngine;
 
 public class DFA : MonoBehaviour
 {
-    string user_input = "aababbababbbaabbaab";
+    /*
+     * Errors with "Invalid # outside" are strings that are less than the states length
+     */
+
+    //Variable for storing user input
+    string user_input = "ba";
 
     void transition_state(string user_input)
     {
@@ -15,123 +20,181 @@ public class DFA : MonoBehaviour
             {
                 case 0:
                     Debug.Log("Initial State");
-                    if (user_input[cursor] == 'a')
+                    if (cursor < user_input.Length - 1)
                     {
-                        cursor++;
-                        goto case 1;
-                    }
-                    else if (user_input[cursor] == 'b')
-                    {
-                        cursor++;
-                        goto case 2;
+
+                        if (user_input[cursor] == 'a')
+                        {
+                            cursor++;
+                            goto case 1;
+                        }
+                        else if (user_input[cursor] == 'b')
+                        {
+                            cursor++;
+                            goto case 2;
+                        }
+                        else
+                        {
+                            Debug.Log("Invalid 0");
+                            return;
+                        }
                     }
                     else
                     {
-                        Debug.Log("Invalid 0");
-                        break;
+                        Debug.Log("Invalid 0 outside");
+                        return;
                     }
                 case 1:
                     Debug.Log("State 2");
-                    if (user_input[cursor] == 'a')
+                    if (cursor < user_input.Length - 1)
                     {
-                        cursor++;
-                        goto case 3;
-                    }
-                    else if (user_input[cursor] == 'b')
-                    {
-                        cursor++;
-                        goto case 2;
+
+                        if (user_input[cursor] == 'a')
+                        {
+                            cursor++;
+                            goto case 3;
+                        }
+                        else if (user_input[cursor] == 'b')
+                        {
+                            cursor++;
+                            goto case 2;
+                        }
+                        else
+                        {
+                            Debug.Log("Invalid 1");
+                            return;
+                        }
                     }
                     else
                     {
-                        Debug.Log("Invalid 1");
-                        break;
+                        Debug.Log("Invalid 1 outside");
+                        return;
                     }
                 case 2:
                     Debug.Log("State 3");
-                    if (user_input[cursor] == 'a' || user_input[cursor] == 'b')
+                    if (cursor < user_input.Length - 1)
                     {
-                        cursor++;
-                        goto case 3;
+
+                        if (user_input[cursor] == 'a' || user_input[cursor] == 'b')
+                        {
+                            cursor++;
+                            goto case 3;
+                        }
+                        else
+                        {
+                            Debug.Log("Invalid 2");
+                            return;
+                        }
                     }
                     else
                     {
-                        Debug.Log("Invalid 2");
-                        break;
+                        Debug.Log("Invalid 2 outside");
+                        return;
                     }
                 case 3:
                     Debug.Log("State 4");
-                    if (user_input[cursor] == 'a')
+                    if (cursor < user_input.Length - 1)
                     {
-                        cursor++;
-                        goto case 4;
-                    }
-                    else if (user_input[cursor] == 'b')
-                    {
-                        cursor++;
-                        goto case 5;
+
+                        if (user_input[cursor] == 'a')
+                        {
+                            cursor++;
+                            goto case 4;
+                        }
+                        else if (user_input[cursor] == 'b')
+                        {
+                            cursor++;
+                            goto case 5;
+                        }
+                        else
+                        {
+                            Debug.Log("Invalid 3");
+                            return;
+                        }
                     }
                     else
                     {
-                        Debug.Log("Invalid 3");
-                        break;
+                        Debug.Log("Invalid 3 outside");
+                        return;
                     }
                 case 4:
                     Debug.Log("State 5");
-                    if (user_input[cursor] == 'a')
+                    if (cursor < user_input.Length - 1)
                     {
-                        cursor++;
-                        goto case 4;
-                    }
-                    else if (user_input[cursor] == 'b')
-                    {
-                        cursor++;
-                        goto case 5;
+
+                        if (user_input[cursor] == 'a')
+                        {
+                            cursor++;
+                            goto case 4;
+                        }
+                        else if (user_input[cursor] == 'b')
+                        {
+                            cursor++;
+                            goto case 5;
+                        }
+                        else
+                        {
+                            Debug.Log("Invalid 4");
+                            return;
+                        }
                     }
                     else
                     {
-                        Debug.Log("Invalid 4");
-                        break;
+                        Debug.Log("Invalid 4 outside");
+                        return;
                     }
                 case 5:
                     Debug.Log("State 6");
-                    //Debug.Log("Cursor: " + cursor);
-                    if (user_input[cursor] == 'a')
+                    if (cursor < user_input.Length - 1)
                     {
-                        cursor++;
-                        goto case 4;
-                    }
-                    else if (user_input[cursor] == 'b')
-                    {
-                        cursor++;
-                        goto case 6;
+
+                        if (user_input[cursor] == 'a')
+                        {
+                            cursor++;
+                            goto case 4;
+                        }
+                        else if (user_input[cursor] == 'b')
+                        {
+                            cursor++;
+                            goto case 6;
+                        }
+                        else
+                        {
+                            Debug.Log("Invalid 5");
+                            return;
+                        }
                     }
                     else
                     {
-                        Debug.Log("Invalid 5");
-                        break;
+                        Debug.Log("Invalid 5 outside");
+                        return;
                     }
                 case 6:
                     Debug.Log("State 7");
-                    if (user_input[cursor] == 'a' || user_input[cursor] == 'b')
+                    if (cursor < user_input.Length - 1)
                     {
-                        cursor++;
-                        goto case 7;
+
+                        if (user_input[cursor] == 'a' || user_input[cursor] == 'b')
+                        {
+                            cursor++;
+                            goto case 7;
+                        }
+                        else
+                        {
+                            Debug.Log("Invalid 6");
+                            return;
+                        }
                     }
                     else
                     {
-                        Debug.Log("Invalid 6");
-                        break;
+                        Debug.Log("Invalid 6 outside");
+                        return;
                     }
                 case 7:
                     Debug.Log("State 8");
                     if (user_input[cursor] == 'a' || user_input[cursor] == 'b' && cursor < user_input.Length - 1)
                     {
                         cursor++;
-                        //Debug.Log("Continue");
-                        //Debug.Log("Cursor value: " + user_input[19]);
-                        //Debug.Log("Cursor: " + cursor);
-                        //Debug.Log("String Length: " + user_input.Length);
                         goto case 7;
                     }
                     else if (user_input[cursor] == 'a' || user_input[cursor] == 'b' && cursor == user_input.Length - 1)
